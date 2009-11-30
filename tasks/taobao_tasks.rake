54b8eb8c4f127126bea61104bfb4bc949ed41db7
David Li <taweili@yahoo.com>
2009-11-30 16:35:33 +0800

    set up skeleton

---
+++ tasks/taobao_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :taobao do
+#   # Task goes here
+# end
---
+++ test/taobao_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class TaobaoTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
